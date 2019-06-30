0ee1ceb3d8dcf4f592845942ebe7f282d07496e0
JoaoRepas <52373559+JoaoRepas@users.noreply.github.com>
2019-06-30 14:30:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>HelloWorld</p>'; ?>
+    <h1>Hello World</h1>
+    <?php echo '<p>This is my first PHP page using Openshift Online</p>'; ?>
   </body>
 </html>
